12fb0e51d9a440d1b5ecea9e515eba01103c822b
amal shyjo <amalshyjo@gmail.com>
2020-06-17 16:38:38 +0530

    Updates

---
+++ precache-manifest.122257dade71e4c7841ba2e16987960e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a635896e99ddb0d7c97c483132d0e0a",
+    "url": "/sheejashyjo/index.html"
+  },
+  {
+    "revision": "454b8bc89c1908d9f2f9",
+    "url": "/sheejashyjo/static/css/main.964406cb.chunk.css"
+  },
+  {
+    "revision": "8ccaab0905c44dded352",
+    "url": "/sheejashyjo/static/js/2.5843805d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sheejashyjo/static/js/2.5843805d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "454b8bc89c1908d9f2f9",
+    "url": "/sheejashyjo/static/js/main.8ca7b114.chunk.js"
+  },
+  {
+    "revision": "0d9e9ebce8c57bdc2d19",
+    "url": "/sheejashyjo/static/js/runtime-main.255ab524.js"
+  }
+]);
